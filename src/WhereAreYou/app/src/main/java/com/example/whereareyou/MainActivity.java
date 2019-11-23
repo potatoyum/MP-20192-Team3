d12d58e85f84bed5d812ac635ee3f2aec255317d
wjh51333 <wjh51333@naver.com>
2019-11-23 18:13:26 +0900

    MainActivity

--- src/WhereAreYou/app/src/main/java/com/example/whereareyou/MainActivity.java
+++ src/WhereAreYou/app/src/main/java/com/example/whereareyou/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.whereareyou;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClickButton(View view) {
+        int id = view.getId();
+        Intent intent;
+
+        switch (id) {
+            case R.id.hospital:
+                intent = new Intent(MainActivity.this, HospitalAct.class);
+                startActivity(intent);
+                break;
+            case R.id.pharmacy:
+                intent = new Intent(MainActivity.this, PharmacyAct.class);
+                startActivity(intent);
+                break;
+            case R.id.shelter:
+                intent = new Intent(MainActivity.this, ShelterAct.class);
+                startActivity(intent);
+                break;
+            case R.id.search:
+                intent = new Intent(MainActivity.this, SearchAct.class);
+                startActivity(intent);
+                break;
+            case R.id.tips:
+                intent = new Intent(MainActivity.this, TipsAct.class);
+                startActivity(intent);
+                break;
+            case R.id.support:
+                intent = new Intent(MainActivity.this, SupportAct.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
