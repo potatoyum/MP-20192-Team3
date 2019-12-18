09c09455248aa1b08576f4a731b80ec508d88e8d
szzi <kamuuim@naver.com>
2019-12-18 16:03:20 +0900

    김서진, 정지우
    google api ~ 지도, 마커
    병원, 약국, 보호센터

---
+++ WhereAreYou_1208 (2)/WhereAreYou_1208/WhereAreYou/WhereAreYou/app/src/main/java/com/example/whereareyou/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.whereareyou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.snackbar.Snackbar;
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton(View view) {
+        int id = view.getId();
+        Intent intent;
+
+        switch (id) {
+            case R.id.hospital:
+                intent = new Intent(MainActivity.this, Hospital.class);
+                startActivity(intent);
+                break;
+            case R.id.pharmacy:
+                intent = new Intent(MainActivity.this, PharmacyAct.class);
+                startActivity(intent);
+                break;
+            case R.id.shelter:
+                intent = new Intent(MainActivity.this, Shelter.class);
+                startActivity(intent);
+                break;
+/*            case R.id.search:
+                intent = new Intent(MainActivity.this, SearchAct.class);
+                startActivity(intent);
+                break;
+            case R.id.tips:
+                intent = new Intent(MainActivity.this, TipsAct.class);
+                startActivity(intent);
+                break;
+            case R.id.support:
+                intent = new Intent(MainActivity.this, SupportAct.class);
+                startActivity(intent);
+                break;*/
+        }
+    }
+}
